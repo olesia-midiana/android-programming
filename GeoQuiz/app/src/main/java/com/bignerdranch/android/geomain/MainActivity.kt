26a922e784ee3003d804b7fc72b93e6c4a1146df
Olesia Midiana <olesia.midiana@gmail.com>
2021-08-25 08:56:20 +0300

    chapter 2 challenge: next, prev buttons as imageButton

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geomain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import android.view.View
@@ -12,8 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
