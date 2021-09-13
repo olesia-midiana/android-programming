d03870a9827a5abf642f18aaef8835959b6b3b62
Olesia Midiana <olesia.midiana@gmail.com>
2021-09-13 18:04:22 +0300

    chapter 9 challenge: RecyclerView ViewTypes

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -79,11 +79,17 @@ class CrimeListFragment : Fragment() {
         : RecyclerView.Adapter<CrimeHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
                 : CrimeHolder {
-            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+
+            val view = if (viewType == 0)
+                layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            else
+                layoutInflater.inflate(R.layout.list_item_serious_crime, parent, false)
             return CrimeHolder(view)
         }
 
         override fun getItemCount() = crimes.size
+
+        override fun getItemViewType (position: Int) = if (crimes[position].requiresPolice) 1 else 0
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
